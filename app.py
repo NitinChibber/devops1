8731d42d4cfa984a256db2d053c1e570926bcb61
Nitin Chibber <Nitin.chibber@gmail.com>
2019-12-10 12:55:35 +0000

    checked reset option

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ print("printline after commit")
 print("New line add")
 new line for questions
 Modified after adding new file app.py.save
+## new line to test undo changes in GIT
+## couple of changes
